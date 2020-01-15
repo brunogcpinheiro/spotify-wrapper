8d37795fd5eabeab88a04397c91d68ace7b9dbdd
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-01-15 15:15:15 +0000

    generic search with one and more types

--- src/__tests__/main.spec.js
+++ src/__tests__/main.spec.js
@@ -29,11 +29,40 @@ describe('Spotify Wrapper', () => {
   });
 
   describe('Generic Search', () => {
-    it('should call fetch function', () => {
-      const fetchedStub = sinon.stub(global, 'fetch');
-      const artists = search();
+    let fetchedStub;
 
+    beforeEach( () => {
+      fetchedStub = sinon.stub(global, 'fetch');
+    });
+
+    afterEach( () => {
+      fetchedStub.restore();
+    });
+
+    it('should call fetch function', () => {
+      const artists = search();
       expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with correct URL', () => {
+      context('passing one type', () => {
+        const artists = search('Incubus', 'artist');
+
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+        const albums = search('Incubus', 'album');
+
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+      });
+
+      context('passing more than one type', () => {
+        const artistsAndAlbum = search('Incubus', ['artist', 'album']);
+
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album');
+      });
     });
   });
 });
